eb5338069d1762c0888d2e9dd3af755b97d74b72
Gabin <hello@gabinaureche.com>
2016-02-25 09:09:20 +0100

    make it work with any value

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 module.exports = function oh (obj, separator = '-') {
+  if (obj == null) return 'null'
+  if (typeof obj === 'number' || typeof obj === 'boolean') return String(obj)
   if (typeof obj === 'string') return obj
+  if (typeof obj === 'function') return obj.name || 'function'
+  if (Array.isArray(obj)) return obj.map((v) => oh(v, separator)).join(separator)
+
+  // we're left with objects
 
   const keys = Object.keys(obj).sort()
 
   return keys
     .map(function (key) {
       let prefix = key
-      let value = obj[key]
-
-      if (Array.isArray(value)) {
-        value =
-          value
-            .map((v) => oh(v))
-            .join(separator)
-      } else if (typeof value === 'object') {
-        value = oh(value)
-      }
+      let value = oh(obj[key], separator)
 
       return ''.concat(prefix, separator, value)
     })
