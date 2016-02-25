1b99025f9d5e0f545d13a22b54165b6195b39b40
Gabin <hello@gabinaureche.com>
2016-02-25 09:18:28 +0100

    add some proper tests

--- index.spec.js
+++ index.spec.js
@@ -3,7 +3,47 @@
 const oh = require('./index')
 
 describe('oh', function () {
-  it('should hashify objects', function () {
+  it('should handle arrays', function () {
+    expect(oh(['foo', 'bar', 'quz'])).toBe('foo-bar-quz')
+  })
+
+  it('should handle objects', function () {
+    expect(oh({ foo: 'bar', quz: 'baz' })).toBe('foo-bar-quz-baz')
+  })
+
+  it('should sort object keys', function () {
+    expect(oh({ z: 'foo', a: 'bar', d: 'quz' })).toBe('a-bar-d-quz-z-foo')
+  })
+
+  it('should handle functions', function () {
+    expect(oh(function foo () {})).toBe('foo')
+    expect(oh(function () {})).toBe('function')
+  })
+
+  it('should handle strings', function () {
+    expect(oh('foo')).toBe('foo')
+  })
+
+  it('should handle null values', function () {
+    expect(oh(null)).toBe('null')
+    expect(oh(undefined)).toBe('null')
+  })
+
+  it('should handle booleans', function () {
+    expect(oh(true)).toBe('true')
+    expect(oh(false)).toBe('false')
+  })
+
+  it('should handle numbers', function () {
+    expect(oh(12)).toBe('12')
+    expect(oh(12.3)).toBe('12.3')
+  })
+
+  it('should support custom separators', function () {
+    expect(oh({ foo: 'bar', quz: 'baz' }, '+')).toBe('foo+bar+quz+baz')
+  })
+
+  it('should handle recursive structures', function () {
     const obj = { zoo: 'bar', qwe: ['foo', { xar: 'boom', abc: { tululu: 'bim' } }], bar: { quz: 'foo', aze: 'qwe', beach: 23 } }
     const expected = 'bar-aze-qwe-beach-23-quz-foo-qwe-foo-abc-tululu-bim-xar-boom-zoo-bar'
 
